765905be307b5942f0a3a55b7535a97ce8e8f668
Richin.C <richin007@outlook.com>
2021-10-09 18:42:07 +0530

    project created

---
+++ RunApp/RunApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunApp
+//
+//  Created by MegaMind on 09/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
